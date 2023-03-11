31687648c9049cd9433e9ca009f1c526fa5c3178
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2023-03-11 05:07:16 +0300

    Add jsonmapper to known issues

--- tests/Feature/CompatibilityTest.php
+++ tests/Feature/CompatibilityTest.php
@@ -130,7 +130,10 @@ class CompatibilityTest extends TestCase
             // Non-const expression in typedef (will not support)
             \str_contains($expr, 'func_num_args() > ') ||
             // PHPDoc bug: https://github.com/phpDocumentor/ReflectionDocBlock/issues/351
-            \str_ends_with($expr, 'string[]}>}|array}|null')
+            \str_ends_with($expr, 'string[]}>}|array}|null') ||
+            // Invalid stmts
+            \str_contains($expr, 'array[string]') ||
+            \str_contains($expr, 'ArrayObject[')
         ) {
             $this->markTestIncomplete("Test is flagged as false-positive:\n" . $message);
         }
