cd94ba998b9a0ecb62344a8e616d55c22e8a94d4
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2023-09-23 05:13:09 +0300

    Make template parameters list iterable

--- src/Node/Stmt/Template/ParametersListNode.php
+++ src/Node/Stmt/Template/ParametersListNode.php
@@ -9,8 +9,10 @@ use TypeLang\Parser\Node\Node;
 /**
  * @internal This is an internal library class, please do not use it in your code.
  * @psalm-internal TypeLang\Parser
+ *
+ * @template-implements \IteratorAggregate<array-key, ParameterNode>
  */
-class ParametersListNode extends Node
+class ParametersListNode extends Node implements \IteratorAggregate, \Countable
 {
     /**
      * @param array<ParameterNode> $list
@@ -18,4 +20,17 @@ class ParametersListNode extends Node
     public function __construct(
         public readonly array $list = [],
     ) {}
+
+    public function getIterator(): \Traversable
+    {
+        return new \ArrayIterator($this->list);
+    }
+
+    /**
+     * @return int<0, max>
+     */
+    public function count(): int
+    {
+        return \count($this->list);
+    }
 }
