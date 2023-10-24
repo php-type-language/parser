d2a5aa2a1e876d13bfde40c023855e653977eaa5
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2023-10-25 02:45:00 +0300

    Add matcher and class matcher visitors

---
+++ src/Traverser/ClassNameMatcherVisitor.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TypeLang\Parser\Traverser;
+
+use TypeLang\Parser\Node\Node;
+
+class ClassNameMatcherVisitor extends MatcherVisitor
+{
+    /**
+     * @param class-string<Node> $class
+     */
+    public function __construct(string $class, ?\Closure $break = null)
+    {
+        $matcher = static fn (Node $node): bool => $node instanceof $class;
+
+        parent::__construct($matcher, $break);
+    }
+}
---
+++ src/Traverser/MatcherVisitor.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TypeLang\Parser\Traverser;
+
+use TypeLang\Parser\Node\Node;
+
+class MatcherVisitor extends Visitor
+{
+    private ?Node $found = null;
+
+    private bool $shouldContinue = false;
+
+    /**
+     * @param \Closure(Node):bool $matcher
+     * @param null|\Closure(Node):bool $break
+     */
+    public function __construct(
+        private readonly \Closure $matcher,
+        private readonly ?\Closure $break = null,
+    ) {}
+
+    public function isFound(): bool
+    {
+        return $this->found !== null;
+    }
+
+    public function getFoundNode(): ?Node
+    {
+        return $this->found;
+    }
+
+    public function before(): void
+    {
+        $this->found = null;
+    }
+
+    public function enter(Node $node): ?Command
+    {
+        if ($this->break) {
+            return Command::SKIP_CHILDREN;
+        }
+
+        if (($this->matcher)($node)) {
+            $this->shouldContinue = true;
+            $this->found = $node;
+
+            return Command::SKIP_CHILDREN;
+        }
+
+        if (($this->break)($node)) {
+            $this->shouldContinue = true;
+
+            return Command::SKIP_CHILDREN;
+        }
+
+        return null;
+    }
+}
