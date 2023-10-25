85ba5df64cb15fe268c681bfc4c7d4d9325368c8
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2023-10-25 23:15:58 +0300

    Improve parser interface description

--- src/ParserInterface.php
+++ src/ParserInterface.php
@@ -5,14 +5,18 @@ declare(strict_types=1);
 namespace TypeLang\Parser;
 
 use Phplrt\Contracts\Source\ReadableInterface;
+use TypeLang\Parser\Exception\ParserExceptionInterface;
 use TypeLang\Parser\Node\Type\TypeStatement;
 
 interface ParserInterface
 {
     /**
+     * Parses variadic sources into an abstract source tree (AST) node.
+     *
      * @param resource|string|\SplFileInfo|ReadableInterface $source
      *
-     * @return TypeStatement|null
+     * @throws ParserExceptionInterface In case of parsing exception occurs.
+     * @throws \Throwable In case of internal error occurs.
      */
     public function parse(mixed $source): ?TypeStatement;
 }
