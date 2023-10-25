5dd0c0e28156a8fc8eb85042577228dc1df2772e
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2023-10-25 23:15:46 +0300

    Simplify callable type node creation

--- src/Node/Type/CallableTypeNode.php
+++ src/Node/Type/CallableTypeNode.php
@@ -11,7 +11,7 @@ class CallableTypeNode extends TypeStatement
 {
     public function __construct(
         public readonly Name $name,
-        public readonly ArgumentsListNode $arguments,
+        public readonly ArgumentsListNode $arguments = new ArgumentsListNode(),
         public readonly ?TypeStatement $type = null,
     ) {}
 }
