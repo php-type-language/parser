499effec0ed95a507a33c224deafc969c5c61605
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2023-11-22 13:34:44 +0300

    Improve rector types

--- src/Traverser/VisitorInterface.php
+++ src/Traverser/VisitorInterface.php
@@ -8,24 +8,11 @@ use TypeLang\Parser\Node\Node;
 
 interface VisitorInterface
 {
-    /**
-     * @return void
-     */
     public function before(): void;
 
-    /**
-     * @return ?Command
-     */
     public function enter(Node $node): ?Command;
 
-    /**
-     *
-     * @return void
-     */
     public function leave(Node $node): void;
 
-    /**
-     * @return void
-     */
     public function after(): void;
 }
