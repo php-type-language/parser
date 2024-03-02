d9a6bade5f386170ba4249484f2210edd4410ffa
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2024-03-03 02:51:44 +0300

    Fix rector config

--- rector.php
+++ rector.php
@@ -2,18 +2,8 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\ClassMethod\LocallyCalledStaticMethodToNonStaticRector;
-use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
-use Rector\CodingStyle\Rector\Assign\SplitDoubleAssignRector;
-use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
-use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
 use Rector\Config\RectorConfig;
-use Rector\EarlyReturn\Rector\Return_\ReturnBinaryOrToEarlyReturnRector;
-use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
-use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\TypeDeclaration\Rector\FunctionLike\AddReturnTypeDeclarationFromYieldsRector;
 
 return static function (RectorConfig $config): void {
     $config->paths([__DIR__ . '/src']);
@@ -21,21 +11,5 @@ return static function (RectorConfig $config): void {
 
     $config->sets([
         LevelSetList::UP_TO_PHP_81,
-        SetList::TYPE_DECLARATION,
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::EARLY_RETURN,
-    ]);
-
-    $config->skip([
-        ClassPropertyAssignToConstructorPromotionRector::class,
-        ReadOnlyPropertyRector::class,
-        CatchExceptionNameMatchingTypeRector::class,
-        SplitDoubleAssignRector::class,
-        FlipTypeControlToUseExclusiveTypeRector::class,
-        ReturnBinaryOrToEarlyReturnRector::class,
-        LocallyCalledStaticMethodToNonStaticRector::class,
-        EncapsedStringsToSprintfRector::class,
-        AddReturnTypeDeclarationFromYieldsRector::class,
     ]);
 };
