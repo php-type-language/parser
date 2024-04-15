0dc50f8221157a076be62344b5a25a1fbf16e15b
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2024-04-15 19:43:41 +0300

    Move tags functional tests from parser package to phpdoc-standard-tags

--- tests/Functional/LinterStubs/phpstorm/2023.3/mongodb/BSON/Persistable.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MongoDB\BSON;
-
-/**
- * Classes may implement this interface to take advantage of automatic ODM (object document mapping) behavior in the driver.
- * @link https://php.net/manual/en/class.mongodb-bson-persistable.php
- */
-interface Persistable extends Unserializable, Serializable {}
--- tests/Functional/LinterStubs/phpstorm/2023.3/mongodb/Exception/SSLConnectionException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MongoDB\Driver\Exception;
-
-/**
- * Thrown when the driver fails to establish an SSL connection with the server.
- * @link https://php.net/manual/en/class.mongodb-driver-exception-sslconnectionexception.php
- */
-class SSLConnectionException extends ConnectionException implements Exception {}
